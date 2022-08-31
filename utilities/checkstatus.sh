e622469821c280aaf8a4a9b3e7600671c76c5d4c
John Lowe <jblowe@berkeley.edu>
2022-08-31 15:19:29 -0700

    CSW-731: refactor checkstatus.sh a bit to better detect pipeline errors

--- utilities/checkstatus.sh
+++ utilities/checkstatus.sh
@@ -7,16 +7,26 @@ for t in bampfa botgarden cinefiles pahma ucjeps
 do
     for d in public internal propagations locations media osteology
     do
-       if [[ -e ${SOLR_LOG_DIR}/${t}.solr_extract_${d}.log ]]
+       LOG=${SOLR_LOG_DIR}/${t}.solr_extract_${d}.log
+       if [[ -e ${LOG} ]]
        then
            CORE="${t}-${d}"
-           DATE=`tail -1 ${SOLR_LOG_DIR}/${t}.solr_extract_${d}.log | perl -ne 'if (/(Sun|Mon|Tue|Wed|Thu|Fri|Sat)/){print}else{print "PROBLEM: log file does not show a date as last line"}'`
+           grep "`tail -1 $LOG | cut -c2-10`" $LOG | perl -ne 'print if /(ERROR|Only 0 rows)/' > ${t}.discrepancies.txt
+           DATE=`tail -1 ${LOG} | perl -ne 'if (/(Sun|Mon|Tue|Wed|Thu|Fri|Sat)/){print}else{print "PROBLEM: log file does not show a date as last line"}'`
            NUMFOUND=`curl -s -S "http://localhost:8983/solr/${t}-${d}/select?q=*%3A*&rows=0&wt=json&indent=true" | grep numFound | perl -pe 's/.*"numFound":(\d+),.*/\1 rows/;'`
-           STATUS=`grep '"status":' ${SOLR_LOG_DIR}/${t}.solr_extract_${d}.log | tail -1 | perl -ne 'unless (/.status.:0/) {print "\nNon-zero status from Solr:\n$_"}'`
-           if [ "-v" == "$1" ] || [ `echo "$DATE" | grep -q "PROBLEM"` ] || [ "$STATUS" != "" ]
+           SOLR_STATUS=`grep '"status":' $LOG | tail -1 | perl -ne 'unless (/.status.:0/) {print "\nNon-zero status from Solr:\n$_"}'`
+           if [ `echo "$DATE" | grep -q "PROBLEM"` ] || [ "$SOLR_STATUS" != "" ] || [ -s ${t}.discrepancies.txt ]
            then
-               echo "$CORE,$DATE,$NUMFOUND $STATUS"
+               STATUS="PROBLEM"
+           else
+               STATUS="OK"
            fi
+           if [ "-v" == "$1" ] || [ "${STATUS}" == "PROBLEM" ]
+           then
+               echo "${STATUS}: $CORE,$DATE,$NUMFOUND $STATUS"
+               cat ${t}.discrepancies.txt
+           fi
+           rm ${t}.discrepancies.txt
        fi
     done
 done
