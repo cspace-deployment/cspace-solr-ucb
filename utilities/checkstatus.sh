47de1e675a91b2c59b41457b43e7c28d5e589b52
John Lowe <jblowe@berkeley.edu>
2022-09-02 12:07:25 -0700

    CSW-731: eliminate some false positives from checkstatus.sh

--- utilities/checkstatus.sh
+++ utilities/checkstatus.sh
@@ -12,7 +12,7 @@ do
        then
            CORE="${t}-${d}"
            # TODO this 'error detection code' could still be a lot better!
-           grep "`tail -1 $LOG | cut -c2-10`" $LOG | perl -ne 'print if /(ERROR|refresh aborted|giving up)/i' > ${t}.discrepancies.txt
+           grep "`tail -1 $LOG | cut -c2-11`" $LOG | perl -ne 'print if (/(ERROR|refresh aborted)/i && ! /(\.csv)/)' > ${t}.discrepancies.txt
            DATE=`tail -1 ${LOG} | perl -ne 'if (/(Sun|Mon|Tue|Wed|Thu|Fri|Sat)/){print}else{print "PROBLEM: log file does not show a date as last line"}'`
            NUMFOUND=`curl -s -S "http://localhost:8983/solr/${t}-${d}/select?q=*%3A*&rows=0&wt=json&indent=true" | grep numFound | perl -pe 's/.*"numFound":(\d+),.*/\1 rows/;'`
            SOLR_STATUS=`grep '"status":' $LOG | tail -1 | perl -ne 'unless (/.status.:0/) {print "\nNon-zero status from Solr:\n$_"}'`
