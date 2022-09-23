df4c7ab252405fc84f4e5a923ca09643507a5357
John Lowe <jblowe@berkeley.edu>
2022-09-23 14:52:28 -0700

    CSW-635: we now stop running cinefiles denorm pipeline

--- utilities/oj.cinefiles.sh
+++ utilities/oj.cinefiles.sh
@@ -2,7 +2,7 @@
 
 source ${HOME}/pipeline-config.sh
 
-# run the 'legacy denormalizing' script first, to populate the 'cinefiles_denorm' db schema used by the portal
-cd ${HOME}/solrdatasources/cinefiles ; /bin/bash -l -c './cinefiles_denorm_nightly.sh' | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/cinefiles.solr_extract_public.log  2>&1 ; cd
-# now run the regular ppipeline that populates solr
+# we RTL no longer run the 'legacy denormalizing' script, LYRASIS does
+# cd ${HOME}/solrdatasources/cinefiles ; /bin/bash -l -c './cinefiles_denorm_nightly.sh' | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/cinefiles.solr_extract_public.log  2>&1 ; cd
+# run the regular pipeline that populates solr
 ${HOME}/solrdatasources/cinefiles/solrETL-public.sh        cinefiles 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/cinefiles.solr_extract_public.log  2>&1
