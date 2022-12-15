87f336c0e823ba9d36194003f811c32a521ff93c
John Lowe <jblowe@berkeley.edu>
2022-12-15 10:47:31 -0800

    CSW-779: update pipeline config for prod to point to lyrasis databases

--- pipeline-config-prod.sh
+++ pipeline-config-prod.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-export BAMPFA_SERVER="dba-postgres-prod-45.ist.berkeley.edu port=5313"
-export BAMPFA_USER="reporter_bampfa"
+export BAMPFA_SERVER="localhost port=54321"
+export BAMPFA_USER="bampfa"
 export BAMPFA_CONTACT="osanchez@berkeley.edu"
 
-export BOTGARDEN_SERVER="dba-postgres-prod-45.ist.berkeley.edu port=5313"
-export BOTGARDEN_USER="reporter_botgarden"
+export BOTGARDEN_SERVER="localhost port=54321"
+export BOTGARDEN_USER="ucbg"
 export BOTGARDEN_CONTACT="ucbg-cspace-bmu@berkeley.edu"
 
-export CINEFILES_SERVER="dba-postgres-prod-45.ist.berkeley.edu port=5313"
-export CINEFILES_USER="reporter_cinefiles"
+export CINEFILES_SERVER="localhost port=54321"
+export CINEFILES_USER="cinefiles"
 export CINEFILES_CONTACT="mcq@berkeley.edu"
 
-# for cinefiles denorm script
-export CINEFILES_PGHOST="dba-postgres-prod-45.ist.berkeley.edu"
-export CINEFILES_PGUSER="nuxeo_cinefiles"
-export CINEFILES_PGPORT=5313
-
-export PAHMA_SERVER="dba-postgres-prod-45.ist.berkeley.edu port=5307"
-export PAHMA_USER="reporter_pahma"
+export PAHMA_SERVER="localhost port=54321"
+export PAHMA_USER="pahma"
 export PAHMA_CONTACT="mtblack@berkeley.edu"
 
-export UCJEPS_SERVER="dba-postgres-prod-45.ist.berkeley.edu port=5310"
-export UCJEPS_USER="reporter_ucjeps"
+export UCJEPS_SERVER="localhost port=54321"
+export UCJEPS_USER="ucjeps"
 export UCJEPS_CONTACT="ucjeps-it@berkeley.edu"
 
 export SUPPORT_CONTACT="cspace-support@berkeley.edu"
