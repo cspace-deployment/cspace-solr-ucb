0c98329e6082c92f32dcd4666d9428b4dd71a3f6
John Lowe <jblowe@berkeley.edu>
2023-02-02 17:18:44 -0800

    CSW-800: mtb's version of 'nagpra repatriation filter'

--- pahma/part48.sql
+++ pahma/part48.sql
@@ -1,34 +1,37 @@
 SELECT cc.id,
        'nagpra' as nagpra_relevant_s
 FROM collectionobjects_common cc
-         LEFT OUTER JOIN collectionobjects_pahma_pahmaobjectstatuslist osl ON (cc.id = osl.id)
-         JOIN misc m ON (m.id = cc.id AND m.lifecyclestate <> 'deleted')
-         JOIN collectionobjects_pahma cp ON (cc.id = cp.id)
+	LEFT OUTER JOIN collectionobjects_pahma_pahmaobjectstatuslist osl ON (cc.id=osl.id)
+	JOIN misc m ON (m.id=cc.id AND m.lifecyclestate='project')
+	JOIN collectionobjects_pahma cp ON (cc.id=cp.id)
+	JOIN collectionobjects_pahma_pahmafieldcollectionplacelist fcp ON (fcp.id=cc.id AND (fcp.pos=0 OR fcp.pos IS NULL))
+	JOIN places_common pc ON (pc.shortidentifier=REGEXP_REPLACE(fcp.item, '^.*item:name\((.*)\)''.*', '\1'))
+	JOIN hierarchy pcsid ON (pc.id=pcsid.id)
+	JOIN utils.placename_hierarchy ON (pcsid.name=utils.placename_hierarchy.placecsid)
 
-WHERE REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1') NOT IN (
-    'not received',
-    'on deposit',
-    'number not used',
-    'deaccessioned',
-    'recataloged')
-AND  regexp_replace(cp.pahmatmslegacydepartment, '^.*\)''(.*)''$', '\1') NOT IN (
-     'Nonnative California (archaeology and ethnology)',
-     'Nonnative US and Canada (except California)',
-     'Loans in',
-     'Archives',
-     'Audio recordings',
-     'Maps',
-     'Registration',
-     '(not assigned)',
-     'South America', -- (except Ancient Peru)
-     'Ancient Peru',
-     'Africa', --  (except Ancient Egypt)'
-     'Ancient Egypt',
-     'Asia', --  (except western Russia)
-     'Still and motion photography',
-     'South America', --  (except Ancient Peru)
-     'Drawings and paintings',
-     'Classical Mediterranean'
-)
+WHERE   osl.item NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaObjectStatuses):item:name(pahmaObjectStatuses05)''deaccessioned''%'
+	AND osl.item NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaObjectStatuses):item:name(pahmaObjectStatuses32)''on deposit''%'
+	AND osl.item NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaObjectStatuses):item:name(pahmaObjectStatuses18)''number not used''%'
+	AND osl.item NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaObjectStatuses):item:name(pahmaObjectStatuses17)''not received''%'
+	AND osl.item NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaObjectStatuses):item:name(pahmaObjectStatuses25)''recataloged''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(NonnativeCaliforniaarchaeologyandethnology1645142411253)''Nonnative California (archaeology and ethnology)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE 'urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(NonnativeUSandCanadaexceptCalifornia1645142411311)''Nonnative US and Canada (except California)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment55)''Loans in''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(Archives1645142411361)''Archives''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment14)''Audio recordings''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment17)''Maps''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment50)''Registration''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment95)''(not assigned)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment06)''South America (except Ancient Peru)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment07)''Ancient Peru''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment08)''Africa (except Ancient Egypt)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment09)''Ancient Egypt''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment12)''Asia (except western Russia)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment18)''Still and motion photography''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment06)''South America (except Ancient Peru)''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment16)''Drawings and paintings''%'
+	AND cp.pahmatmslegacydepartment NOT LIKE '%urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(pahmaTmsLegacyDepartments):item:name(pahmaTMSLegacyDepartment11)''Classical Mediterranean''%'
+	AND (  utils.placename_hierarchy.place_hierarchy ILIKE '%United States%'
+        OR utils.placename_hierarchy.place_hierarchy ILIKE '%Hawaii%')
 
 GROUP BY cc.id;
