7b891da05cffd35b8d44360a5ca6eb369b4ad492
Lam Voong <lkv@berkeley.edu>
2024-06-24 11:24:17 -0700

    CSW-908: remove references to osteology to retire osteology portal

--- utilities/oj.pahma.sh
+++ utilities/oj.pahma.sh
@@ -5,4 +5,3 @@ source ${HOME}/pipeline-config.sh
 ${HOME}/solrdatasources/pahma/solrETL-public.sh            pahma     2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/pahma.solr_extract_public.log  2>&1
 ${HOME}/solrdatasources/pahma/solrETL-internal.sh          pahma     2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/pahma.solr_extract_internal.log  2>&1
 ${HOME}/solrdatasources/pahma/solrETL-locations.sh         pahma     2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/pahma.solr_extract_locations.log  2>&1
-# ${HOME}/solrdatasources/pahma/solrETL-osteology.sh         pahma     2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${SOLR_LOG_DIR}/pahma.solr_extract_osteology.log  2>&1
--- utilities/optimize.sh
+++ utilities/optimize.sh
@@ -6,7 +6,6 @@ time curl -S -s http://localhost:8983/solr/botgarden-public/update --data '<opti
 time curl -S -s http://localhost:8983/solr/cinefiles-public/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
 time curl -S -s http://localhost:8983/solr/pahma-internal/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
 time curl -S -s http://localhost:8983/solr/pahma-locations/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
-time curl -S -s http://localhost:8983/solr/pahma-osteology/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
 time curl -S -s http://localhost:8983/solr/pahma-public/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
 time curl -S -s http://localhost:8983/solr/ucjeps-media/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
 time curl -S -s http://localhost:8983/solr/ucjeps-public/update --data '<optimize/>' -H 'Content-type:text/xml; charset=utf-8'
