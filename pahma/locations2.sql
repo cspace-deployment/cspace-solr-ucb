1db993ccecdefcbe7789a8f5e9845ea6ed3679a2
Lam Voong <lkv@berkeley.edu>
2024-10-04 16:08:20 -0700

    HMP-292: update queries for objectcount (nubmerofobjects), objectcountnote (inventorycount)

--- pahma/locations2.sql
+++ pahma/locations2.sql
@@ -1,17 +1,31 @@
-SELECT
-   csid.name AS object_csid_s,
-   -- csid.id as id,
-   -- REGEXP_REPLACE(co.computedcurrentlocation, '^.*\)''(.*)''$', '\1') AS storagelocation_s,
-   -- REGEXP_REPLACE(ca.computedcrate, '^.*\)''(.*)''$', '\1') AS computedcrate_s,
-   co.objectnumber AS objectnumber_s,
-   cp.sortableobjectnumber as sortableobjectnumber_s,
-   co.numberofobjects AS "objectcount_s",
-   ong.objectName AS objectname_s, 
-   cp.inventorycount as count_s
-FROM collectionobjects_common co
-JOIN hierarchy csid ON (co.id = csid.id)
-LEFT OUTER JOIN hierarchy h1 ON (co.id = h1.parentid AND h1.pos=0 AND h1.name='collectionobjects_common:objectNameList')
-LEFT OUTER JOIN objectnamegroup ong ON (ong.id=h1.id)
-LEFT OUTER JOIN collectionobjects_pahma cp ON (co.id=cp.id)
-LEFT OUTER JOIN collectionobjects_anthropology ca ON (co.id=ca.id)
-JOIN misc ms ON (co.id=ms.id AND ms.lifecyclestate <> 'deleted')
+SELECT DISTINCT on (cc.objectnumber)
+    hcc.name                                AS object_csid_s,
+    -- cc.id                                AS id,
+    -- GETDISPL(cc.computedcurrentlocation) AS storagelocation_s,
+    -- GETDISPL(ca.computedcrate)           AS computedcrate_s,
+    cc.objectnumber                         AS objectnumber_s,
+    cp.sortableobjectnumber                 AS sortableobjectnumber_s,
+    ocg.objectcount                         AS objectcount_s,
+    ong.objectName                          AS objectname_s, 
+    ocg.objectcountnote                     AS count_s
+FROM collectionobjects_common cc
+    JOIN misc m ON (
+        cc.id = m.id
+        AND m.lifecyclestate <> 'deleted')
+    JOIN hierarchy hcc ON (cc.id = hcc.id)
+    LEFT OUTER JOIN hierarchy hong ON (
+        cc.id = hong.parentid 
+        AND hong.pos = 0 
+        AND hong.primarytype = 'objectNameGroup')
+    LEFT OUTER JOIN objectnamegroup ong ON (hong.id = ong.id)
+    LEFT OUTER JOIN collectionobjects_pahma cp ON (cc.id = cp.id)
+    LEFT OUTER JOIN collectionobjects_anthropology ca ON (cc.id = ca.id)
+    LEFT OUTER JOIN hierarchy hocg ON (
+        cc.id = hocg.parentid
+        AND hocg.primarytype = 'objectCountGroup')
+    LEFT OUTER JOIN objectcountgroup ocg ON (hocg.id = ocg.id)
+WHERE getdispl(ocg.objectcounttype) = 'piece count'
+ORDER BY 
+    cc.objectnumber,
+    ocg.objectcountdate
+    DESC NULLS LAST;
